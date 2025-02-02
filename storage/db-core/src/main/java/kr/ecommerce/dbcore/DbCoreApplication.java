95fff44cdf327747e7791e54f864222809676a9f
gimjinseong <wlsdiqkdrk@naver.com>
2025-02-03 00:04:31 +0900

    feature: 멀티 모듈 세팅

---
+++ storage/db-core/src/main/java/kr/ecommerce/dbcore/DbCoreApplication.java
@@ -0,0 +1,13 @@
+package kr.ecommerce.dbcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbCoreApplication.class, args);
+    }
+
+}
