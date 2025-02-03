e7479165247fd4bc9179292d99a13b7862130e96
gimjinseong <wlsdiqkdrk@naver.com>
2025-02-03 23:01:30 +0900

    feature: 프로젝트 세팅 - 멀티 모듈, 공통 에러 핸들링과 BaseEntity

---
+++ domain/src/main/java/kr/ecommerce/domain/support/error/SimpleMessageIllegalStateException.java
@@ -0,0 +1,24 @@
+package kr.ecommerce.domain.support.error;
+
+import lombok.Getter;
+
+@Getter
+public class SimpleMessageIllegalStateException extends IllegalStateException {
+    private final String message;
+    private final String field;
+    private final Object[] parameters;
+
+    public SimpleMessageIllegalStateException(String message) {
+        this(message, null);
+    }
+
+    public SimpleMessageIllegalStateException(String message, String field) {
+        this(message, field, null);
+    }
+
+    public SimpleMessageIllegalStateException(String message, String field, Object[] parameters) {
+        this.message = message;
+        this.field = field;
+        this.parameters = parameters;
+    }
+}
